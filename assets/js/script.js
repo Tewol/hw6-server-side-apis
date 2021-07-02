914084266d138f6b82211fb1b008510204c94a51
Hewan Redie <tewol22@gmail.com>
2021-07-02 10:30:16 -0500

    conversion and adding special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,8 @@ var apiKey = "0846fd7d71c2829c591944b48eaefebf";
 var apiUrlCitySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 var apiFutureForcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=" + numDays + "&appid=" + apiKey
 
-
-var searchFormEl = document.querySelector('#search-form');
-
 var current = moment().format("MM/DD/YYYY");
-
+ 
 
 var dayOne = moment().add(1, 'days').format("MM/DD/YYYY");
 var dayTwo = moment().add(2, 'days').format("MM/DD/YYYY");
@@ -18,8 +15,6 @@ var dayFour = moment().add(4, 'days').format("MM/DD/YYYY");
 var dayFive = moment().add(5, 'days').format("MM/DD/YYYY");
 
 
-$("#currentDay").text(current);
-
 function getCityName() {
   fetch(apiUrlCitySearch)
     .then(function (response) {
@@ -27,19 +22,17 @@ function getCityName() {
     })
     .then(function (data) {
 
-      console.log(data);
+      $("h2").append (data.name + ' (' ); 
+      $("h2").append (current + ')' + '&mdash;'); 
 
-      $("h2").append (data.name); 
-    
-      //const celsius = kelvin - 273;
-      //const fahrenheit = Math.floor(celsius * (9/5) + 32);
-      $("#temp").append(data.main.temp)
-      $("#wind").append(data.wind.speed)
-      $("#humidity").append(data.main.humidity) //humidity
+      kelvin = data.main.temp;
+      const celsius = kelvin - 273;
+      const fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+      $("#temp").append(fahrenheit + '&deg;' + "F") 
+      $("#wind").append(data.wind.speed + " MPH")
+      $("#humidity").append(data.main.humidity + ' &#37;' )
       $("#uvIndex").append(data.weather[0].icon) 
-
-      
-    
   })
   
 }
@@ -52,6 +45,7 @@ function getFutureWeather() {
     .then(function (data) {
 
       console.log(data)
+      
 
       //Future 5 Days 
       $("#dayOne").append(data.list[0].dt_txt).text (dayOne)
@@ -60,6 +54,8 @@ function getFutureWeather() {
       $("#dayFour").append(data.list[3].dt_txt).text (dayFour)
       $("#dayFive").append(data.list[4].dt_txt).text (dayFive)
 
+
+      //Future 5 Days Icon
       iconOne = data.list[0].weather[0].icon;
       var iconUrl = "http://openweathermap.org/img/w/" + iconOne + ".png";
       $("#dayOneIcon").html("<img src='" + iconUrl  + "'>");
@@ -80,29 +76,54 @@ function getFutureWeather() {
       var iconUrl = "http://openweathermap.org/img/w/" + iconFive + ".png";
       $("#dayFiveIcon").html("<img src='" + iconUrl  + "'>");
 
-    
       
-     
       //Temperature forcast for the next 5 days
-      $("#dayOneTemp").append(data.list[0].main.temp)
-      $("#dayTwoTemp").append(data.list[1].main.temp)
-      $("#dayThreeTemp").append(data.list[2].main.temp)
-      $("#dayFourTemp").append(data.list[3].main.temp)
-      $("#dayFiveTemp").append(data.list[4].main.temp)
+      for (i=0; i<5; i++){
+        kelvin = data.list[i].main.temp
+        
+        var celsius = kelvin - 273;
+        console.log(kelvin);
+
+        var fahrenheit = [Math.floor(celsius * (9/5) + 32)]
+
+        console.log(fahrenheit);
+
+        if (fahrenheit[0]){
+          $("#dayOneTemp").append(fahrenheit[0] + '&deg;' + "F")
+        }
+
+        if (fahrenheit[1]){
+          $("#dayTwoTemp").append(fahrenheit[1] + '&deg;' + "F")
+        }
+
+        if(fahrenheit[2]){
+          $("#dayThreeTemp").append(fahrenheit[2] + '&deg;' + "F")
+        }
+        
+        if(fahrenheit[3]){
+          $("#dayFourTemp").append(fahrenheit[3] + '&deg;' + "F")
+        }
+        
+        if(fahrenheit[4]){
+          $("#dayFiveTemp").append(fahrenheit[4] + '&deg;' + "F")
+        }
+      
+
+      }
 
       //Wind forcast for the next 5 days
-      $("#dayOneWind").append(data.list[0].wind.deg)
-      $("#dayTwoWind").append(data.list[1].wind.deg)
-      $("#dayThreeWind").append(data.list[2].wind.deg)
-      $("#dayFourWind").append(data.list[3].wind.deg)
-      $("#dayFiveWind").append(data.list[4].wind.deg)    
+      $("#dayOneWind").append(data.list[0].wind.speed + " MPH")
+      $("#dayTwoWind").append(data.list[1].wind.speed + " MPH")
+      $("#dayThreeWind").append(data.list[2].wind.speed + " MPH")
+      $("#dayFourWind").append(data.list[3].wind.speed + " MPH")
+      $("#dayFiveWind").append(data.list[4].wind.speed + " MPH")    
 
       //Humidity forcast for the next 5days
-      $("#dayOneHumidity").append(data.list[0].main.humidity)
-      $("#dayTwoHumidity").append(data.list[1].main.humidity)
-      $("#dayThreeHumidity").append(data.list[2].main.humidity)
-      $("#dayFourHumidity").append(data.list[3].main.humidity)
-      $("#dayFiveHumidity").append(data.list[4].main.humidity)
+      $("#dayOneHumidity").append(data.list[0].main.humidity + ' &#37;' )
+      $("#dayTwoHumidity").append(data.list[1].main.humidity + ' &#37;' )
+      $("#dayThreeHumidity").append(data.list[2].main.humidity + ' &#37;' )
+      $("#dayFourHumidity").append(data.list[3].main.humidity + ' &#37;' )
+      $("#dayFiveHumidity").append(data.list[4].main.humidity + ' &#37;' )
   })
   
 }
@@ -111,21 +132,8 @@ function storeInput() {
   city = $("#search-input").val();
   localStorage.setItem("search", city);
   console.log(city);
+
 }
-
-
-function searchFormSubmit() {
-  //event.preventDefault();
-  var searchInputVal = document.querySelector('#search-input').value;
-  console.log(searchInputVal)
-  if (!searchInputVal) {
-    console.error('You need to enter City!');
-    return;
-  }
-    console.log(city);
-    searchApi(searchInputVal);
-}
-
 
 
 $("#searchBtn").on("click", storeInput)
